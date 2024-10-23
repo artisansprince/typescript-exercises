59dd637b5f98b217e282487acf25194eccea62fa
artisansprince <artisansprince@gmail.com>
2024-10-24 05:57:17 +0700

    🌱 feat: menambahkan tantangan.

---
+++ src/2024-10-24-find-missing-number.ts
@@ -0,0 +1,23 @@
+/*2024-10-24-find-missing-number.ts
+
+
+Tantangan: Find the Missing Number
+Diberikan sebuah array yang berisi angka-angka dari 1 hingga n dengan satu angka yang hilang. Temukan angka yang hilang tersebut.
+
+Input:
+Array nums yang berisi angka dari 1 hingga n (dengan satu angka yang hilang).
+Contoh: [1, 2, 4, 5, 6]
+
+Output:
+Angka yang hilang dari array tersebut.
+Contoh: 3
+
+Langkah Penyelesaian:
+Cari jumlah total dari angka 1 hingga n menggunakan rumus n * (n + 1) / 2.
+Hitung jumlah semua angka dalam array.
+Kurangkan jumlah total dengan jumlah dari array untuk menemukan angka yang hilang.
+
+
+*/
+
+
