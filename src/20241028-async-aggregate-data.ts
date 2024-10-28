a873ab6ffa7653566dbb2eb8882dc768feae2d11
artisansprince <artisansprince@gmail.com>
2024-10-28 08:23:32 +0700

    🌱 feat: menambahkan tantangan.

---
+++ src/20241028-async-aggregate-data.ts
@@ -0,0 +1,15 @@
+/*20241028-async-aggregate-data.ts
+
+Tantangan Async Hari Ini: "Async Aggregate Data"
+Buatlah fungsi yang menggunakan async/await untuk mengambil data array objek dengan struktur yang mirip transaksi finansial, lalu:
+1. Filter data dengan transaksi type: "income" saja.
+2. Hitung total jumlah amount dari transaksi bertipe "income" tersebut.
+3. Return total tersebut.
+
+
+Langkah-langkah
+1. Ambil data transaksi secara asynchronous (bisa data dummy atau dari API).
+2. Gunakan await untuk memastikan semua langkah (filter dan aggregate) selesai sebelum fungsi mengembalikan hasil.
+3. Output berupa total amount untuk semua transaksi bertipe "income".
+
+*/
