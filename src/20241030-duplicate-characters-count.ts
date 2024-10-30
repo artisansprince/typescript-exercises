f7925cc6d076bdf3bbf14ac6c30320738a06cdad
artisansprince <artisansprince@gmail.com>
2024-10-30 10:35:08 +0700

    🌱 feat: menambahkan tantangan.

---
+++ src/20241030-duplicate-characters-count.ts
@@ -0,0 +1,21 @@
+/* 20241030-duplicate-characters-count.ts
+
+Tantangan hari ini: Duplicate Characters Count
+Buat fungsi yang bisa menghitung jumlah karakter duplikat dalam sebuah string. Misalnya, diberikan string "programming", maka hasilnya adalah 3 (karakter yang berulang: r, g, m).
+
+Langkah Tantangannya:
+Buat fungsi countDuplicates(str: string): number.
+Loop atau gunakan metode lain untuk menghitung setiap karakter yang muncul lebih dari sekali dalam string.
+Return jumlah karakter unik yang memiliki duplikat.
+Contoh Input dan Output:
+Input: "programming"
+
+Output: 3 (karena karakter r, g, dan m muncul lebih dari sekali)
+
+Input: "typescript"
+
+Output: 1 (karena hanya karakter t yang muncul dua kali)
+
+
+*/
+
